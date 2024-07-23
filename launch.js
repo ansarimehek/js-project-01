cd37d9475720381a67b9ade617f108f2b11badab
unknown <ansarimehek10@gmail.com>
2024-07-23 17:59:05 +0530

    not done

--- launch.js
+++ launch.js
@@ -1,7 +1,42 @@
-window.onload = function() {
-    var seconds = 00;
-    var tens = 00;
-    var appendTens = document.getElementById ("tens");
-    var appendSeconds = document.CDATA_SECTION_NODE 
-    
+let startTime;
+let running = false;
+let laps = [];
+let lapcount = 1;
+let interval;
+
+document.getElementById('btn-start').addEventListener('click', startStopwatch);
+document.getElementById('btn-stop').addEventListener('click', stopStopwatch);
+document.getElementById('btn-reset').addEventListener('click', resetStopwatch);
+
+function startStopwatch() {
+    intervalId = setInterval(() => {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes === 60) {
+            hours++;
+            minutes = 0;
+        }
+        updateStopwatchDisplay();
+    }, 1000);
+}
+
+function resetStopwatch() {
+    clearInterval(intervalId);
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    updateStopwatchDisplay();
+}
+
+function updateStopwatchDisplay() {
+    document.getElementById('hours').innerText = padZero(hours);
+    document.getElementById('minutes').innerText = padZero(minutes);
+    document.getElementById('seconds').innerText = padZero(seconds);
+}
+
+function padZero(time) {
+    return (time < 10) ? '0' + time : time;
 }
